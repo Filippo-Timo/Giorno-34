e037fc8fe5ad0e3d5fbe08fc9bf523e605860265
Filippo-Timo <filtimo2002@gmail.com>
2025-11-06 16:54:49 +0100

    Updates

---
+++ pexels-start.js
@@ -0,0 +1,44 @@
+const imagesURL = "https://api.pexels.com/v1/search?query=hamsters";
+const apiKey = "mWOK34O6lk6XoMG2iaBWqFzIUEXs1U3chpZpQ4CPHkKnF0BToljJXiXr";
+const secondButtonImgs = "https://api.pexels.com/v1/search?query=tigers";
+
+const changeImages = function () {
+  fetch(imagesURL, {
+    headers: {
+      //   "Content-Type": "application/json",
+      Authorization: apiKey,
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        console.log("Evvai", res);
+        return res.json();
+      } else {
+        throw new Error(`Errore nell'if: ${res.status}`);
+      }
+    })
+    .then((singleObj) => {
+      const loadButton = document.getElementById("loadButton");
+      const loadSecondary = document.getElementById("loadSecondaryButton");
+      let allImgs = document.querySelectorAll("img");
+      loadButton.addEventListener("click", (e) => {
+        singleObj.photos.forEach((element, index) => {
+          if (allImgs[index]) {
+            allImgs[index].src = element.src.original;
+          }
+        });
+      });
+      loadSecondary.addEventListener("click", (e) => {
+        singleObj.photos.forEach((element, index) => {
+          if (allImgs[index]) {
+            allImgs[index].src = element.src.original;
+          }
+        });
+      });
+    })
+    .catch((err) => {
+      console.log("ERRORE NEL CATCH:", err);
+    });
+};
+
+changeImages();
